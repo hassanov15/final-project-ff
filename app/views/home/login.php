a49448a8d4f2eca8b8e5c24da9f97a7243837e01
hassanov15 <hassanov-15@hotmail.com>
2017-12-02 21:54:41 -0500

    the login views
    creat the login page

---
+++ app/views/home/login.php
@@ -0,0 +1,21 @@
+<div class="container center">
+    <div class="row">
+        <div class="col-md-offset-5 col-md-3">
+            <form method="post" action="../app/controllers/login_controller.php">
+                <div class="form-login">
+                <h4>Welcome back.</h4>
+                <input type="text" id="userName" class="form-control input-sm chat-input" name='username' placeholder="username" />
+                </br>
+                <input type="password" id="userPassword" class="form-control input-sm chat-input" name="password" placeholder="password" />
+                </br>
+                <div class="wrapper">
+            <span class="group-btn">
+                <button type="submit" name="login" class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></button>
+            </span>
+                </div>
+            </div>
+            </form>
+
+        </div>
+    </div>
+</div>
