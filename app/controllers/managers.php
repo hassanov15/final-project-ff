307f53381f39b6b8fed60932107936101b531e7e
hassanov15 <hassanov-15@hotmail.com>
2017-12-02 23:56:47 -0500

    it it does exists
    print the one it exists

--- app/controllers/managers.php
+++ app/controllers/managers.php
@@ -4,3 +4,10 @@ $q =$_REQUEST["q"];
 $options ="";
 $sql = "select user_name_id from users where type='manager'";
 $result= myqli_query($con,$sql);
+if($result == mysqli_fetch_assoc($row)){
+	$options= $row['user_name_id'].$row['user_name'];
+
+
+}
+echo $options 
+?>
