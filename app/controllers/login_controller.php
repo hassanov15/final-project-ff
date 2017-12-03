34ef2471b725d9584cc7d48b0e7e4a310e3fcce0
hassanov15 <hassanov-15@hotmail.com>
2017-12-02 21:38:22 -0500

    the login
    creating username and pawword, and giving the ability to recohnize the admin.

--- app/controllers/login_controller.php
+++ app/controllers/login_controller.php
@@ -1,1 +1,14 @@
 <?php
+
+
+if(isset($_POST['login'])){
+	$username = $_POST['username'];
+	$password = $_post['password'];
+
+
+	if($username ==="admin"&& $password === "admim"){
+		$_SESSION['username']="admin";
+		$_SESSION['user']= "admin";
+		header('location:')
+	}
+}
