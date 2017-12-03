ecdc4b970910be93a5c15950ab401d6fac8845e8
hassanov15 <hassanov-15@hotmail.com>
2017-12-02 22:56:12 -0500

    fixed the footer.
    i removed the all unneeded codes, put the style and the place for my copy right.

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,30 +1,6 @@
 <!DOCTYPE html>
-<html>
-<body>
-
-<footer class="footer">    
-    <div class="row">
-    	<div class="col-lg-12">
-
-  <p>Contact information for any help: <a href="mailto:someone@example.com">hassan@hotmail.com</a>.</p>
+<footer style="margin-top: 100px;">
+	<div class="text-center">
+		Copyright &copy: Hassan
+	</div>
 </footer>
-
-
-
-</html>
-
-
-<footer class="footer">    
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
-        </div>
-    </div>
-</footer>
-
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
-</body>
-</html>
