5af83c27e4d55d9dd77b42b9ae7ec31276ed9863
hassanov15 <hassanov-15@hotmail.com>
2017-12-04 01:58:43 -0500

    find out
    find if the name already exists and direct them to thier assign manger

--- app/controllers/name.php
+++ app/controllers/name.php
@@ -8,6 +8,16 @@ $sqlmangerid = "select manager_id from managers where user_id=$user_id";
 $result = mysqli_query($con,$sqlmanagerid);
 $row = mysqli_fetch_assoc($result);
 $sqlmanagerName ="Select user_name from users where user_id=".$row['manger_id'];
+$result = mysqli_query($con,$sqlmanagerName);
+$rowManger = mysqli_fetch_assoc($result);
+$sql= "select client_name from clients where client_name='$name'";
+$result = mysqli_query($con,$sql);
+if(mysqli_num_rows($result) > 0){
+    echo "Name Already Exists ! Plaese contact ".$rowManager['user_name']." For further assistance";
+}else{
+
+}
+?>
 
 
 
