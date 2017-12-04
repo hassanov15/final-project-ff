551d9f0631d0dcb7b292ef82d389a2719bbf2a86
hassanov15 <hassanov-15@hotmail.com>
2017-12-04 01:24:38 -0500

    managers
    go to the manager table, find the provided id, if it match

--- app/controllers/name.php
+++ app/controllers/name.php
@@ -1,2 +1,11 @@
-<php 
+<?php 
+$name = $_REQUEST['q'];
 include 'connect.php';
+
+$user_id = $_SESSION['id'];
+
+$sqlmangerid = "select manager_id from managers where user_id=$user_id";
+$result = mysqli_query($con,$sqlmanagerid);
+$row = mysqli_fetch_assoc($result);
+
+
