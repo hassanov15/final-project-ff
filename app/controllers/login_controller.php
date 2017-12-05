db55341457c335fd836253f41ee5511188756cf5
hassanov15 <hassanov-15@hotmail.com>
2017-12-04 21:34:03 -0500

    fixed

--- app/controllers/login_controller.php
+++ app/controllers/login_controller.php
@@ -9,12 +9,19 @@ if(isset($_POST['login'])){
 	if($username ==="admin"&& $password === "admim"){
 		$_SESSION['username']="admin";
 		$_SESSION['user']= "admin";
-		header('location:')
+		header('location:public/')
 	}else{
 	
-	$sql = "select * from users where user_name='$username' and user_password='$password'";
-	 $result = mysqli_query($con, $sql);
-	 
+	 $sql = "select * from users where user_name='$username' and user_password='$password'";
+        $result = mysqli_query($con, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['username'] = $row['user_name'];
+            $_SESSION['user'] = $row['type'];
+            $_SESSION['id'] = $row['user_id'];
+            header('location:public/');
+
+        }
 
 	}
 }
