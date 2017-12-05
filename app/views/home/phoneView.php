9645025013e27ea9738fee6c9fa00d7f357902bc
hassanov15 <hassanov-15@hotmail.com>
2017-12-04 19:39:09 -0500

    the phoneView view
    conated to the table to connect it to the onwer of the number

--- app/views/home/phoneView.php
+++ app/views/home/phoneView.php
@@ -1,2 +1,28 @@
+<div class="container">
+    <h2>Phone Directory</h2>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Designation</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $sql = "select * from users";
+                    $result = mysqli_query($con,$sql);
+                    $i = 1;
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo "<tr><td>".$i."</td><td>".$row['user_name']."</td><td>".$row['phone']."</td><td>".$row['type']."</td></tr>";
+                        $i++;
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 
